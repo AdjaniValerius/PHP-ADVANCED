53b0a81c19445952c6e468d2155a9302605c102f
Adjani Valerius <2068687@talnet.nl>
2020-06-23 11:48:16 +0100

    taak 1 klaar

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+$Student = array(
+    "voornaam" => "piet",
+    "achternaam" => "jan",
+    "leeftijd" => "15",
+    "klas" => "9C"
+);
+
+echo " Deze student heet " . $Student['voornaam'] . $Student['achternaam'] . " , hij is " . $Student['leeftijd'] . " jaar oud en zit in klas " . $Student['klas'];
+
+$Mohammad = array(
+    "voornaam" => "Mohammad",
+    "achternaam" => "ali",
+    "leeftijd" => "14",
+    "klas" => "9S"
+);
+ 
+$Els = array(
+    "voornaam" => "Els",
+    "achternaam" => "dijk",
+    "leeftijd" => "19",
+    "klas" => "5F"
+);
+
+?>
