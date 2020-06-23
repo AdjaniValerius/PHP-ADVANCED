1e170fa3bc2b43b3e3ac15159533f83ed2582645
Adjani Valerius <2068687@talnet.nl>
2020-06-23 16:57:27 +0100

    2-taak4, nog bezig

---
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -0,0 +1,20 @@
+<?php
+
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+
+    foreach($klassen =;$klassen;$klassen){
+        echo "Ik zit in klas: ";
+    }
+
+?>
+
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="Style.css">
+    </head>
+
+    <body>
+        <button>VERZENDEN</button>   
+    </body>
+</html>
