14bb43558150a69b7a677c8c416f01bf68867eb1
Adjani Valerius <2068687@talnet.nl>
2020-06-23 14:45:10 +0100

    2-taak2

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,8 @@
+<?php
+
+    for($getal = 0; $getal <= 18; $getal++){
+        echo "<li>" . "Ik ben $getal jaar, dus ik mag nog niet stemmen <li/>";
+    }
+    echo "Ik ben 18 jaar oud dus ik heb stemrecht!";
+
+?>
