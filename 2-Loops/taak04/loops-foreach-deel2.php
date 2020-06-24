12c0eb01e99c563e3b62098e17075546d3076743
Adjani Valerius <2068687@talnet.nl>
2020-06-24 11:41:52 +0100

    2-taak4

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -2,10 +2,6 @@
 
 $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
 
-    foreach($klassen =;$klassen;$klassen){
-        echo "Ik zit in klas: ";
-    }
-
 ?>
 
 
@@ -15,6 +11,28 @@ $klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
     </head>
 
     <body>
-        <button>VERZENDEN</button>   
+    <form mehtod="POST">
+    <select name="klas"> 
+    <option value=""></option>
+    <?php
+    foreach ($klassen as $klas) {
+        echo "<option value='". $klas . "'> ". $klas . "</option>";
+        }
+    ?>
+
+    </select>
+    <input type="submit" value="verzenden">
+    </form>
+
+    <ul>
+    <li value=""></li>
+        <?php
+            foreach ($klassen as $klas) {
+            echo "<li> ". $klas . "</li>";
+        }
+    ?>
+
+    </ul>
+
     </body>
 </html>
