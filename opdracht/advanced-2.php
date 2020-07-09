57688f150f2db9a2919e78b3ecb94eea35815864
Adjani Valerius <2068687@talnet.nl>
2020-07-09 14:03:00 +0100

    eindopdracht bezig

---
+++ opdracht/advanced-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <link rel="stylesheet" type="text/css" href="Style.css">
+        <script src="script.js"></script>
+        <title></title>
+
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
